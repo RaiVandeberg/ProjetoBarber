2a69e7a4e8db90ecb59d023197bb3facc073a051
Raí <devvandenberg>
2025-01-19 01:14:40 -0300

    mudando a porta

--- ProjectBarber/src/modules/form/date-change.js
+++
@@ -1,7 +0,0 @@
-import { schedulesDay } from '../schedules/load'
-
-// Seleciona o input da data
-const selectedDate = document.getElementById('date')
-
-// Recarrega a lista de horários ao alterar a data
-selectedDate.onchange = () => schedulesDay()
--- ProjectBarber/src/modules/page-load.js
+++
@@ -1,5 +0,0 @@
-import { schedulesDay } from './schedules/load'
-
-document.addEventListener('DOMContentLoaded', () => {
-  schedulesDay()
-})
